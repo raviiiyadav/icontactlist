71e1c99ac11eb5a1b4902d7b8d8bd5e9e5e2ce2f
ravii <raviiyadav.job@gmail.com>
2023-03-05 22:15:37 +0530

    commit5

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/inotebook?readPreference=primary&appName=MongoDB%25Compass&directConnection=true&tls=false"
+const mongoURI = "mongodb://127.0.0.1:27017/icontactlist?readPreference=primary&appName=MongoDB%25Compass&directConnection=true&tls=false"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
